ba8028c112029a032875a3ee6de93fcbeeff0038
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-10-27 13:34:43 -0400

    lecture work-in-progress

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -3,11 +3,7 @@ import axios from 'axios';
 
 class App extends React.Component {
     state = {
-        dogs:[
-            "https://images.dog.ceo/breeds/cattledog-australian/IMG_0206.jpg",
-            "https://images.dog.ceo/breeds/cattledog-australian/IMG_1042.jpg",
-            "https://images.dog.ceo/breeds/cattledog-australian/IMG_1062.jpg"
-        ]
+        dogs:[]
     }
     
     render() {
