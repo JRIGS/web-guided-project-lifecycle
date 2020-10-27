82461ea6d8272af1ad890e148e17560b54cdb4e7
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-10-27 13:23:06 -0400

    lecture work-in-progress

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import axios from 'axios';
+
+class App extends React.Component {
+    render() {
+        return(
+            <div className="App">
+                <h1>Search Doooogs</h1>
+            </div>
+        )
+    }
+}
