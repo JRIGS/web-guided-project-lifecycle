7d052f85cb715119bc9b5ec0dea85841fc81e05b
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-10-27 13:23:55 -0400

    lecture work-in-progress

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -2,10 +2,17 @@ import React from 'react';
 import axios from 'axios';
 
 class App extends React.Component {
+    state = {
+        dogs:[]
+    }
+    
     render() {
         return(
             <div className="App">
                 <h1>Search Doooogs</h1>
+                <div className="dogContainer">
+
+                </div>
             </div>
         )
     }
